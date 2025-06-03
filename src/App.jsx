be569038a9df5162621bfd7d68735ffd68d13485
nforn22 <fornarinicolas22@gmail.com>
2025-06-03 12:27:13 +0200

    Bonus: Crossed-out tasks should be displayed at the bottom of the list

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,11 @@ function App() {
     <div className="container">
       <h1>Todo List</h1>
       <TaskForm onAdd={addTask} />
-      <TaskList tasks={tasks} onToggle={toggleTask} onDelete={deleteTask} />
+      <TaskList
+        tasks={[...tasks].sort((a, b) => a.isDone - b.isDone)}
+        onToggle={toggleTask}
+        onDelete={deleteTask}
+      />
       <footer>
         Made with React at <a href="https://www.lereacteur.io/?utm_source=google&utm_medium=cpc&utm_campaign=FR-SN-Brand&utm_term=le%20r%C3%A9acteur&utm_content=377851505166&gad_source=1&gad_campaignid=1695930647&gbraid=0AAAAADP5alZKMyHuOuTI1KKWg1L1awHgr&gclid=CjwKCAjwl_XBBhAUEiwAWK2hzttLXfZRp2jJqvV8pj_gWlS6j3Bs5Mlme02-8LgAxlt5Ewy-pVu5RRoCvcYQAvD_BwE">Le Reacteur</a> by <a href="https://github.com/nforn22">nforn22</a>
       </footer>
