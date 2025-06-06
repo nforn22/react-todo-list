530476adee4e04b58acaefd2d0b1489d383e5f2f
nforn22 <fornarinicolas22@gmail.com>
2025-06-06 11:00:01 +0200

    feat: add dark mod toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import TaskForm from "./components/Task/TaskForm";
 import TaskList from "./components/Task/TaskList";
+import { GrSun } from "react-icons/gr";
+import { FaMoon } from "react-icons/fa";
 import './App.css'
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
 
   const addTask = (text) => {
@@ -27,10 +30,29 @@ function App() {
     setTasks(tasks.filter(task => task.id !== id));
   };
 
+  const filteredTasks = tasks.filter((task) =>
+    task.text.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="container">
+    // <div className="container">
+    <div className={isDarkMode ? "container dark" : "container"}>
       <h1>Todo List</h1>
-      <TaskForm onAdd={addTask} />
+
+      <button 
+        className={`mode-toggle ${isDarkMode ? 'dark-active' : ''}`}
+        onClick={() => setIsDarkMode(!isDarkMode)}
+      >
+        <div className="toggle-content">
+          <div className="icon-container">
+            {isDarkMode ? <FaMoon size={20} /> : <GrSun size={20} />}
+          </div>
+          <div className="text">
+            {isDarkMode ? 'NIGHTMODE' : 'DAYMODE'}
+          </div>
+        </div>
+      </button>
+    
       <input
         type="text"
         placeholder="Search task..."
@@ -39,6 +61,8 @@ function App() {
         className="search-input"
       />
 
+      <TaskForm onAdd={addTask} />
+      <TaskList tasks={filteredTasks} onToggle={toggleTask} onDelete={deleteTask} />
       <TaskList
         tasks={
         [...tasks]
@@ -50,7 +74,7 @@ function App() {
         onToggle={toggleTask}
         onDelete={deleteTask}
       />
-      
+
       <footer>
         Made with React at <a href="https://www.lereacteur.io/?utm_source=google&utm_medium=cpc&utm_campaign=FR-SN-Brand&utm_term=le%20r%C3%A9acteur&utm_content=377851505166&gad_source=1&gad_campaignid=1695930647&gbraid=0AAAAADP5alZKMyHuOuTI1KKWg1L1awHgr&gclid=CjwKCAjwl_XBBhAUEiwAWK2hzttLXfZRp2jJqvV8pj_gWlS6j3Bs5Mlme02-8LgAxlt5Ewy-pVu5RRoCvcYQAvD_BwE">Le Reacteur</a> by <a href="https://github.com/nforn22">nforn22</a>
       </footer>
